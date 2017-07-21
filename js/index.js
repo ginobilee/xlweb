d76f6b68a4e692950e4060db2b8ade7465670625
ginobilee <ginobilee@users.noreply.github.com>
2017-07-21 10:30:39 +0800

    products section done

--- js/index.js
+++ js/index.js
@@ -1,17 +1,58 @@
 //if show the first screen
 var fSShow = true;
 var details = document.querySelector('#details')
-console.log(document.body.offsetTop)
-console.log(details.offsetTop)
+var navUl = document.querySelector('#navUl')
+var offsetTops = {},intervalId
+initiateOffsetTops();
 
 document.addEventListener('mousewheel',function(e){
-	//console.log('wheelDelta and deltaY:'+e.wheelDelta+','+e.deltaY)
 	var scrollDown = e.deltaY>0?true:false
 	if(scrollDown && fSShow){
+		details.classList.remove('slideBottom')
 		details.classList.add('slideTop')
+		details.focus()
+		fSShow = false
 		return 
 	}
-	console.log(document.body.offsetTop)
-	console.log(document.body.scrollTop)
-    console.log(details.offsetTop)
+    if(!fSShow && details.scrollTop === 0 && !scrollDown){
+    	details.classList.remove('slideTop')
+    	details.classList.add('slideBottom')
+    	fSShow = true
+    	return
+    }
 },false);
+
+navUl.addEventListener('click',function(e){
+	e.preventDefault()
+	var liId = e.target.getAttribute('href')
+	var liClicked = document.querySelector(liId)
+	if(details.classList.contains('slideTop')){
+		smoothSlide(details,details.scrollTop,offsetTops[liId.slice(1)])
+	}
+},false)
+function initiateOffsetTops(){
+	var cNodes = Array.prototype.slice.call(details.childNodes)
+	var eles = cNodes.filter(function(item){
+		return item.nodeType ===1
+	})
+	var id
+	for(var i=0,n=eles.length;i<n;i++){
+		id = eles[i].getAttribute('id')
+		offsetTops[id] = eles[i].offsetTop
+	}
+}
+function smoothSlide(ele,start,stop){
+	var increment = stop>start?5:-5,counter=0
+
+	if(!intervalId){
+	intervalId = window.setInterval(function(){
+		ele.scrollTop += increment*5
+		counter++
+		if(counter === Math.floor(Math.abs(start-stop)/5)){
+			ele.scrollTop = stop
+			clearInterval(intervalId)
+			intervalId = null
+		}
+	},1)
+	}
+}
